dec002385bd67407f0cc635769c1ee3847babee0
i.kłusek <J6ZkqwVUn>
2023-11-05 13:17:01 +0100

    NS-01-Project structure

---
+++ src/lib/components/Left.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<div class="wrapper">
+	<h1>Form</h1>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		flex: 1;
+	}
+</style>
---
+++ src/lib/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Right } from './Right.svelte';
+export { default as Left } from './Left.svelte';
