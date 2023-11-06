52e5fa796c9ef0d4b7e1d90368ca9080f27e960f
i.kłusek <J6ZkqwVUn>
2023-11-06 08:55:42 +0100

    NS-02-Newsletter UI

--- src/lib/components/Left.svelte
+++ src/lib/components/Left.svelte
@@ -1,12 +1,20 @@
 <script>
+	import { Form, NewsLetter } from '$components';
 </script>
 
 <div class="wrapper">
-	<h1>Form</h1>
+	<NewsLetter />
+	<Form />
 </div>
 
 <style lang="scss">
 	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 60px;
 		flex: 1;
+		padding: 10px 20px;
 	}
 </style>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Right } from './Right.svelte';
 export { default as Left } from './Left.svelte';
+export { default as NewsLetter } from './NewsLetter.svelte';
+export { default as Form } from './Form.svelte';
